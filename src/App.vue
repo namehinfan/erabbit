e963b9bda2cff75d6fee2b6bd6b6d95430b06f9f
namehinfan <pd5350488@gmail.com>
2023-02-13 01:11:35 +0800

    请求函数封装

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
-    App
+    {{ $store.state.user.profile.account }}
+    <button @click="$store.commit('user/setUser',{account:'zy'})">设置用户信息</button>
   </div>
 </template>
+
+<script>
+export default {
+  setup () {
+  }
+}
+</script>
 
 <style lang="less">
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,9 @@
 import { createStore } from 'vuex'
+import createPersistedstate from 'vuex-persistedstate'
+
+import cart from './modules/cart'
+import user from './modules/user'
+import category from './modules/category'
 
 export default createStore({
   state: {
@@ -10,5 +15,14 @@ export default createStore({
   actions: {
   },
   modules: {
-  }
+    cart,
+    user,
+    category
+  },
+  plugins: [
+    createPersistedstate({
+      key: 'erabbit-client-pc-store',
+      paths: ['user', 'cart']
+    })
+  ]
 })
