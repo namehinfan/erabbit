879607d4ba690283db607628d8286bf3b64899e0
namehinfan <pd5350488@gmail.com>
2023-02-23 02:52:40 +0800

    分类

--- src/apis/category.js
+++ src/apis/category.js
@@ -4,3 +4,21 @@ import request from '@/utils/request'
 export const findAllCategory = () => {
   return request('/home/category/head', 'get')
 }
+
+/**
+ *  获取顶级类目信息
+ * @param {String} id -顶级类目
+ * @returns
+ */
+export const findTopCategory = (id) => {
+  return request('/category', 'get', { id })
+}
+
+/**
+ *  获取二级类目筛选数据
+ * @param {String} id -二级类目
+ * @returns
+ */
+export const findSubCategoryFilter = (id) => {
+  return request('/category/sub/filter', 'get', { id })
+}
---
+++ src/views/category/components/sub-bread.vue
@@ -0,0 +1,41 @@
+<template>
+  <XtxBread>
+    <XtxBreadItem to="/">首页</XtxBreadItem>
+    <XtxBreadItem v-if="category.top" :to="`/category/${category.top.id}`">{{category.top.name}}</XtxBreadItem>
+    <Transition name="fade-right" mode="out-in">
+      <XtxBreadItem v-if="category.sub" :key="category.sub.id">{{category.sub.name}}</XtxBreadItem>
+    </Transition>
+  </XtxBread>
+</template>
+
+<script>
+import { computed } from '@vue/reactivity'
+import { useRoute } from 'vue-router'
+import { useStore } from 'vuex'
+
+export default {
+  name: 'SubBread',
+  setup () {
+    const route = useRoute()
+    const store = useStore()
+    const category = computed(() => {
+      const cate = {}
+      store.state.category.list.forEach(top => {
+        if (top.children) {
+          const sub = top.children.find(sub => sub.id === route.params.id)
+          if (sub) {
+            cate.top = { id: top.id, name: top.name }
+            cate.sub = { id: sub.id, name: sub.name }
+          }
+        }
+      })
+      return cate
+    })
+    return { category }
+  }
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
